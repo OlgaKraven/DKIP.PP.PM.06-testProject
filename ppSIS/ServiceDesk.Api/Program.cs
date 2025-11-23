c354843bfa6cb90325a326dc502d407fa2637a00
OlgaKraven <129203943+OlgaKraven@users.noreply.github.com>
2025-11-23 23:21:42 +0300

    Задание 3

--- ppSIS/ServiceDesk.Api/Program.cs
+++ ppSIS/ServiceDesk.Api/Program.cs
@@ -1,21 +1,33 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;      // ← ОБЯЗАТЕЛЬНО
-using ServiceDesk.Api.Data;              // ← чтобы видеть AppDbContext
+using ServiceDesk.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Контроллеры
 builder.Services.AddControllers();
 
-// регистрируем DbContext с InMemory
+// Подключение PostgreSQL через EF Core
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("ServiceDeskDb"));
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+});
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Автоматическое создание БД (для учебного примера)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 if (app.Environment.IsDevelopment())
 {
